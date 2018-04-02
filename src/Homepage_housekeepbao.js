71837ad5c5be55c01ec59b7b8634d98dab72667e
EricJhang <housekeepbao@gmail.com>
2018-04-02 10:35:29 +0800

    [EricJK] add Headers

---
+++ src/Homepage_housekeepbao.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import fetch from 'node-fetch';
+import Header from './components/Header/Header';
+import'./css/custom.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './images/logo/logo.jpg'
+import {
+  getFromStorage,
+  setInStorage,
+} from './utils/storage';
+
+class Homepage_housekeepbao extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            count:0
+        }
+    };
+    addCount = () => {
+        const newCount = this.state.count += 1;
+        this.setState({count:newCount});
+    }
+    render(){
+        return (
+            <div>
+                <Header/>
+                <div className="mytest">
+                <h1>{this.state.count}</h1>
+                <button onClick={this.addCount}>add count</button>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Homepage_housekeepbao;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import fetch from 'node-fetch';
+import'./Header.css'
+import logoimg from './logo.jpg'
+// The Header creates links that can be used to navigate
+// between routes.
+const pageHeader =  {
+    position: "relative",
+    width: "100%",
+    zIndex: 1000,
+    padding: 0,
+    margin: 0,
+    border: 0,
+    color: "#425d74",
+    boxShadow:"5px 1px #cfcece"
+  };
+
+const Header = () => (
+    <header className="pageHeader header-sticky">
+        <div className="pageHeaderTop">
+            <div className="container">
+                <div className="logo">
+                    <img src={ logoimg}  width={80} height={80} alt=""></img>
+                    <div className="shine"></div>
+                </div>
+                <div className="quote-button-wrap"><a href={""} className="btn"><i className="icon icon-bell"></i>Login</a></div>
+                <a href="#" className="menu-toggle"><i className="icon-line-menu"></i><i className="icon-cancel"></i></a>
+            </div>
+        </div>
+        <div className="page-header-menu">
+            <div className="container">
+                <ul className="menu">
+                    <li className="active"><Link to="/">首頁</Link></li>
+                    <li><Link to="">關於我們</Link></li>
+                    <li><Link to="">服務<span className="arrow"></span></Link>
+                        <ul className="sub-menu">
+                            <li><Link to="">套房清潔</Link></li>
+                            <li><Link to="">家庭清潔</Link></li>
+                            <li><Link to="">大掃除</Link></li>
+                        </ul>
+                    </li>
+                    <li><Link to ="">常見問題</Link></li>
+                    <li><Link to="">部落格<span className="arrow"></span></Link></li>
+                    <li><Link to="">成為家事員</Link></li>	
+                    <li><Link to="">聯絡我們</Link></li>	
+                </ul>
+            </div>
+        </div>
+    </header>
+)
+
+export default Header
